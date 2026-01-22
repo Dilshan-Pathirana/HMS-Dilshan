9a39af5463e5db3a39ebbf607d5a95bfd7110b21
Dilshan Pathirana <s19121@sci.pdn.ac.lk>
2026-01-22 17:26:49 +0530

    Fix Vite build: add react-slick and slick-carousel

--- resources/frontend/src/main.tsx
+++ resources/frontend/src/main.tsx
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./store.tsx";
 import "react-toastify/dist/ReactToastify.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
